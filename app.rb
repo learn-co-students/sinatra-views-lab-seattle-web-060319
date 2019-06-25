45d88212582d1cc83d08d4470c71625440aec0ae
Renier De La Cruz <me@renierdelacruz.com>
2019-06-25 17:55:42 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,7 +1,17 @@
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index
+	get '/hello' do
+		erb :hello
+	end
+	
+	get '/goodbye' do
+	  @name = "Joe"
+	  erb :goodbye
+	end
+	
+	get '/date' do
+	  @today = Time.now.strftime("%A, %B %e, %Y")
+	  erb :date
 	end
 
 
