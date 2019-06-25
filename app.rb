339526034668f51af8fcda543f308a1a98fb8e7d
Dex Fitch <fitch.dexter@gmail.com>
2019-06-24 17:24:42 -0700

    rotorouter

--- app.rb
+++ app.rb
@@ -4,5 +4,18 @@ class App < Sinatra::Base
 		erb :index
 	end
 
+  get '/hello' do
+    erb :hello
+  end
+
+  get '/goodbye' do
+    @name = "Joe"
+    erb :goodbye
+  end
+
+  get '/date' do
+    @today = Time.now.strftime("%A, %B %e, %Y")
+    erb :date
+  end
 
 end
